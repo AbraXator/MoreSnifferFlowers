18594bbeccf3c57cc3ae061b7061b738a3567a66
AbraXator <adammek.mack@gmail.com>
2024-03-12 13:16:27 +0100

    banner pattern

--- src/main/java/net/abraxator/moresnifferflowers/data/ModRecipesProvider.java
+++ src/main/java/net/abraxator/moresnifferflowers/data/ModRecipesProvider.java
@@ -24,6 +24,6 @@ public class ModRecipesProvider extends RecipeProvider {
         trimSmithing(pRecipeOutput, ModItems.AROMA_ARMOR_TRIM_SMITHING_TABLE.get(), MoreSnifferFlowers.loc(getItemName(ModItems.AROMA_ARMOR_TRIM_SMITHING_TABLE.get()) + "_smithing_trim"));
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.AROMA_ARMOR_TRIM_SMITHING_TABLE.get(), 2).pattern("ABA").pattern("ACA").pattern("AAA").define('A', Items.DIAMOND).define('B', ModItems.AROMA_ARMOR_TRIM_SMITHING_TABLE.get()).define('C', ModTags.ModItemTags.AROMA_TRIM_TEMPLATE_INGREDIENT).unlockedBy("has_aroma_trim_template", has(ModItems.AROMA_ARMOR_TRIM_SMITHING_TABLE.get())).save(pRecipeOutput);
         ShapedRecipeBuilder.shaped(RecipeCategory.MISC, ModItems.DYESPRIA.get()).pattern("AA").pattern("BA").define('A', Items.PAPER).define('B', ModBlocks.CAULORFLOWER.get().asItem()).unlockedBy("has_caulorflower", has(ModBlocks.CAULORFLOWER.get())).save(pRecipeOutput);
-        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.AMBUSH_BANNER_PATTERN).requires(Items.PAPER).requires(ModItems.AMBER_SHARD).unlockedBy("has_amber_shard", has(ModItems.AMBER_SHARD));
+        ShapelessRecipeBuilder.shapeless(RecipeCategory.MISC, ModItems.AMBUSH_BANNER_PATTERN).requires(Items.PAPER).requires(ModItems.AMBER_SHARD).unlockedBy("has_amber_shard", has(ModItems.AMBER_SHARD)).save(pRecipeOutput);
     }
 }
