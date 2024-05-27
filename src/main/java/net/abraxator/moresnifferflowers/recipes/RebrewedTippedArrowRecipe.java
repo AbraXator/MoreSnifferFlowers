ede1d462710968e5ff30c83e16e4c7316bcfed14
AbraXator <adammek.mack@gmail.com>
2024-05-27 18:31:06 +0200

    tipped arrow recipe

--- src/main/java/net/abraxator/moresnifferflowers/recipes/RebrewedTippedArrowRecipe.java
+++ src/main/java/net/abraxator/moresnifferflowers/recipes/RebrewedTippedArrowRecipe.java
@@ -8,11 +8,11 @@ import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
-import net.minecraft.world.item.crafting.TippedArrowRecipe;
 import net.minecraft.world.level.Level;
 
-public class RebrewedTippedArrowRecipe extends TippedArrowRecipe {
+public class RebrewedTippedArrowRecipe extends CustomRecipe {
     public RebrewedTippedArrowRecipe(CraftingBookCategory pCategory) {
         super(pCategory);
     }
@@ -54,7 +54,12 @@ public class RebrewedTippedArrowRecipe extends TippedArrowRecipe {
             return itemstack1;
         }
     }
-
+    
+    @Override
+    public boolean canCraftInDimensions(int pWidth, int pHeight) {
+        return pWidth >= 2 && pHeight >= 2;
+    }
+    
     @Override
     public RecipeSerializer<?> getSerializer() {
         return ModRecipeSerializers.REBREWED_TIPPED_ARROW.get();
