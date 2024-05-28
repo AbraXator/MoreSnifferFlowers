41e96da922e9db5a75e1b6db41aa50ef776ad595
AbraXator <adammek.mack@gmail.com>
2024-05-28 17:45:32 +0200

    .

---
+++ src/main/java/net/abraxator/moresnifferflowers/worldgen/features/ModTreeFeatures.java
@@ -0,0 +1,11 @@
+package net.abraxator.moresnifferflowers.worldgen.features;
+
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+
+public class ModTreeFeatures {
+    public static final ResourceKey<ConfiguredFeature<?, ?>> CORRUPTED_TREE = FeatureUtils.createKey("corrupted_tree");
+    
+    
+}
