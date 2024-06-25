caa3030f0d35fa520fb73a6ae8f43bf42daa59de
Nikdo53 <usivatel@gmail.com>
2024-06-25 11:08:02 +0200

    Barvičky v1

--- src/main/java/net/abraxator/moresnifferflowers/blocks/ColorableVivicusBlock.java
+++ src/main/java/net/abraxator/moresnifferflowers/blocks/ColorableVivicusBlock.java
@@ -48,21 +48,21 @@ public interface ColorableVivicusBlock extends Colorable {
     default Map<DyeColor, Integer> colorValues() {
         return Util.make(Maps.newLinkedHashMap(), dyeColorHexFormatMap -> {
             dyeColorHexFormatMap.put(DyeColor.WHITE, 0xFFFFFFFF);
-            dyeColorHexFormatMap.put(DyeColor.LIGHT_GRAY, 0xFF9d979b);
-            dyeColorHexFormatMap.put(DyeColor.GRAY, 0xFF474f52);
-            dyeColorHexFormatMap.put(DyeColor.BLACK, 0xFF1d1d21);
-            dyeColorHexFormatMap.put(DyeColor.BROWN, 0xFF835432);
-            dyeColorHexFormatMap.put(DyeColor.RED, 0xFFb5432e);
-            dyeColorHexFormatMap.put(DyeColor.ORANGE, 0xFFf89635);
-            dyeColorHexFormatMap.put(DyeColor.YELLOW, 0xFFffee53);
-            dyeColorHexFormatMap.put(DyeColor.LIME, 0xFF80c71f);
-            dyeColorHexFormatMap.put(DyeColor.GREEN, 0xFF5e7c16);
-            dyeColorHexFormatMap.put(DyeColor.CYAN, 0xFF00AACC);
-            dyeColorHexFormatMap.put(DyeColor.LIGHT_BLUE, 0xFF70d9e4);
-            dyeColorHexFormatMap.put(DyeColor.BLUE, 0xFF4753ac);
-            dyeColorHexFormatMap.put(DyeColor.PURPLE, 0xFFb15fc2);
-            dyeColorHexFormatMap.put(DyeColor.MAGENTA, 0xFFd276b9);
-            dyeColorHexFormatMap.put(DyeColor.PINK, 0xFFf8b0c4);
+            dyeColorHexFormatMap.put(DyeColor.LIGHT_GRAY, 0xFFe1e0e0);
+            dyeColorHexFormatMap.put(DyeColor.GRAY, 0xFFbdbdbd);
+            dyeColorHexFormatMap.put(DyeColor.BLACK, 0xFF888888);
+            dyeColorHexFormatMap.put(DyeColor.BROWN, 0xFFcba886);
+            dyeColorHexFormatMap.put(DyeColor.RED, 0xFFf3c1c1);
+            dyeColorHexFormatMap.put(DyeColor.ORANGE, 0xFFf8d5ba);
+            dyeColorHexFormatMap.put(DyeColor.YELLOW, 0xFFf8f6bc);
+            dyeColorHexFormatMap.put(DyeColor.LIME, 0xFFcef69c);
+            dyeColorHexFormatMap.put(DyeColor.GREEN, 0xFF9ce4a5);
+            dyeColorHexFormatMap.put(DyeColor.CYAN, 0xFF8df3db);
+            dyeColorHexFormatMap.put(DyeColor.LIGHT_BLUE, 0xFFaaefed);
+            dyeColorHexFormatMap.put(DyeColor.BLUE, 0xFF93c1d8);
+            dyeColorHexFormatMap.put(DyeColor.PURPLE, 0xFFbfa3f2);
+            dyeColorHexFormatMap.put(DyeColor.MAGENTA, 0xFFd8a9f2);
+            dyeColorHexFormatMap.put(DyeColor.PINK, 0xFFfacbf9);
         });
     }
 }
