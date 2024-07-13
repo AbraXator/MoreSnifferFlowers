8b4574c9ac37500a18d9bf552651114faf7ed814
AbraXator <adammek.mack@gmail.com>
2024-07-13 11:56:56 +0200

    mrtky?

--- src/main/java/net/abraxator/moresnifferflowers/lootmodifers/AddItemsModifier.java
+++ src/main/java/net/abraxator/moresnifferflowers/lootmodifers/AddItemsModifier.java
@@ -7,6 +7,7 @@ import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.abraxator.moresnifferflowers.init.ModItems;
 import net.minecraft.Util;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.ExtraCodecs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,6 +19,7 @@ import net.neoforged.neoforge.common.loot.LootModifier;
 import net.neoforged.neoforge.server.command.ModIdArgument;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
@@ -25,6 +27,9 @@ public class AddItemsModifier extends LootModifier {
     public static final Supplier<Codec<AddItemsModifier>> CODEC = Suppliers.memoize(()
             -> RecordCodecBuilder.create(inst -> codecStart(inst).and(ExtraCodecs.nonEmptyList(BuiltInRegistries.ITEM.byNameCodec().listOf())
             .fieldOf("item").forGetter(m -> m.items)).apply(inst, AddItemsModifier::new)));
+    public static final List<ResourceLocation> SNIFFERENT_ITEMS_LOC = List.of(snifferentLoc("spindlefern_seeds"), snifferentLoc("spineflower_seeds"), snifferentLoc("lumibulb_seeds"), snifferentLoc("sniffberry_seedling"), snifferentLoc("bloom_plant_nut"), snifferentLoc("globar_sapling"), snifferentLoc("club_moss_patch"), snifferentLoc("amber"));
+    public static final List<ResourceLocation> HELLIONS_ITEMS_LOC = List.of(hellionsLoc("stone_pine_sapling"), hellionsLoc("fiddlefern"), hellionsLoc("ivy"));
+    public static final List<ResourceLocation> QUARK_ITEMS_LOC = List.of(new ResourceLocation("quark", "ancient_sapling"));
 
     private final List<Item> items;
 
@@ -43,16 +48,41 @@ public class AddItemsModifier extends LootModifier {
             }
         }
 
-        if (generatedLoot.stream().anyMatch(itemStack -> itemStack.is(Items.TORCHFLOWER_SEEDS))) {
-            generatedLoot.add(Items.PITCHER_POD.getDefaultInstance());
-        } else {
-            generatedLoot.add(Items.TORCHFLOWER_SEEDS.getDefaultInstance());
-        }
+        generatedLoot.clear();
+        generatedLoot.add(Items.PITCHER_POD.getDefaultInstance());
+        generatedLoot.add(Items.TORCHFLOWER_SEEDS.getDefaultInstance());
+        modSupport(SNIFFERENT_ITEMS_LOC, generatedLoot);
+        modSupport(HELLIONS_ITEMS_LOC, generatedLoot);
+        modSupport(QUARK_ITEMS_LOC, generatedLoot);
 
         items.forEach(item -> generatedLoot.add(item.getDefaultInstance()));
         newLoot.add(Util.getRandom(generatedLoot, context.getRandom()));
-        newLoot.add(Util.getRandom(generatedLoot, context.getRandom()));
         return newLoot;
+    }
+
+    private void modSupport(List<ResourceLocation> itemsLocList, ObjectArrayList<ItemStack> generatedLoot) {
+        List<ItemStack> itemList = new ArrayList<>();
+
+        itemsLocList.forEach(resourceLocation -> {
+            var item = BuiltInRegistries.ITEM.get(resourceLocation);
+            if(!item.getDefaultInstance().is(Items.AIR)) {
+                itemList.add(item.getDefaultInstance());
+            }
+        });
+
+        if(itemList.isEmpty()) {
+            return;
+        }
+
+        generatedLoot.addAll(itemList);
+    }
+
+    private static ResourceLocation snifferentLoc(String path) {
+        return new ResourceLocation("snifferent", path);
+    }
+
+    private static ResourceLocation hellionsLoc(String path) {
+        return new ResourceLocation("snifferplus", path);
     }
 
     @Override
